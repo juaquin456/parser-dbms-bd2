4128e7bbae4bc376b09993d462ec4e94f2e3a5eb
Enrique <enrique.flores@utec.edu.pe>
2023-08-31 00:45:14 -0500

    gitignore

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/juaquin/Documentos/UTEC/Ciclo6/BaseDatos2/projects/Proyecto1/parser/CPP
-# Build directory: /home/juaquin/Documentos/UTEC/Ciclo6/BaseDatos2/projects/Proyecto1/parser/CPP/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(basic "/home/juaquin/Documentos/UTEC/Ciclo6/BaseDatos2/projects/Proyecto1/parser/CPP/test/test0.pl")
-set_tests_properties(basic PROPERTIES  WORKING_DIRECTORY "/home/juaquin/Documentos/UTEC/Ciclo6/BaseDatos2/projects/Proyecto1/parser/CPP/cmake-build-debug" _BACKTRACE_TRIPLES "/home/juaquin/Documentos/UTEC/Ciclo6/BaseDatos2/projects/Proyecto1/parser/CPP/CMakeLists.txt;33;add_test;/home/juaquin/Documentos/UTEC/Ciclo6/BaseDatos2/projects/Proyecto1/parser/CPP/CMakeLists.txt;0;")
--- driver.hpp
+++ driver.hpp
@@ -1,36 +1,38 @@
 #ifndef __DRIVER_HPP__
 #define __DRIVER_HPP__
 
-#include <string>
 #include <cstddef>
 #include <istream>
+#include <string>
 #include <unordered_set>
 
-#include "scanner.hpp"
 #include "parser.tab.hh"
+#include "scanner.hpp"
 
 struct column_t;
 
-class driver{
+class driver {
 public:
-    driver();
+  driver();
 
-    virtual ~driver();
+  virtual ~driver();
 
-    void parse(const char* filename);
+  void parse(const char *filename);
 
-    void parse(std::istream &iss);
+  void parse(std::istream &iss);
 
-    void insert();
-    void del();
-    void exec();
-    void createTable(std::string& tablename, const std::vector<column_t*>& columns);
-    void select(std::string& tablename, std::vector<std::string>* column_names);
+  void insert();
+  void del();
+  void exec();
+  void createTable(std::string &tablename,
+                   const std::vector<column_t *> &columns);
+  void select(std::string &tablename, std::vector<std::string> *column_names);
+
 private:
-    void parse_helper(std::istream &stream);
-    std::unordered_set<std::string> tablenames;
-    yy::parser *parser = nullptr;
-    scanner *sc = nullptr;
+  void parse_helper(std::istream &stream);
+  std::unordered_set<std::string> tablenames;
+  yy::parser *parser = nullptr;
+  scanner *sc = nullptr;
 };
 
 #endif
