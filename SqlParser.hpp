39e186755fa5fa6fd4d291457bc24b9bcd84553a
Enrique <enrique.flores@utec.edu.pe>
2023-09-02 20:43:39 -0500

    Concept example

--- SqlParser.hpp
+++ SqlParser.hpp
@@ -1,17 +1,26 @@
 #ifndef SQL_PARSER_HPP
 #define SQL_PARSER_HPP
 
+#include <concepts>
 #include <cstddef>
 #include <istream>
 #include <string>
 #include <unordered_set>
+#include <vector>
 
 #include "parser.tab.hh"
 #include "scanner.hpp"
 
 struct column_t;
 
-class SqlParser {
+template <typename T>
+concept engine_t = requires(T t) {
+  {
+    t.createTable(std::vector<std::string>(), std::string())
+  } -> std::convertible_to<void>;
+};
+
+template <engine_t T> class SqlParser {
 public:
   SqlParser();
 
@@ -24,11 +33,17 @@ public:
   void insert();
   void del();
   void exec();
+
   void createTable(std::string &tablename,
                    const std::vector<column_t *> &columns);
-  void select(std::string &tablename, std::vector<std::string> *column_names);
+
+  void select(vect<str> &response_body, std::string &tablename,
+              std::vector<std::string> *column_names) {
+    response_body = engine.select(tablename, column_names);
+  }
 
 private:
+  T engine;
   void parse_helper(std::istream &stream);
   std::unordered_set<std::string> tablenames;
   yy::parser *parser = nullptr;
